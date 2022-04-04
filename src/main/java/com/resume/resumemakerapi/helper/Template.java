b349d90b07acd010482cc3f5c90340cd327a07f5
Chetan Gupta <59758751+chetanguptamrt@users.noreply.github.com>
2022-04-04 09:38:27 +0530

    update

--- src/main/java/com/resume/resumemakerapi/helper/Template.java
+++ src/main/java/com/resume/resumemakerapi/helper/Template.java
@@ -2,7 +2,10 @@ package com.resume.resumemakerapi.helper;
 
 import java.io.File;
 import java.io.IOException;
+// import java.io.InputStream;
 import java.nio.file.Files;
+
+// import com.resume.resumemakerapi.ResumeMakerApiApplication;
 
 import org.springframework.core.io.ClassPathResource;
 
@@ -10,6 +13,10 @@ public class Template {
 
     static {
         try {
+            // InputStream resourceAsStream = ResumeMakerApiApplication.class.getClassLoader().getResourceAsStream("static/head-data.html");
+            // byte[] ba = new byte[resourceAsStream.available()];
+            // resourceAsStream.read(ba);
+            // HEAD = new String(ba);
             HEAD = new String(Files.readAllBytes(java.nio.file.Paths.get(new ClassPathResource("static").getFile().getAbsolutePath()+File.separator+"head-data.html")));
         } catch (IOException e) {
             e.printStackTrace();
